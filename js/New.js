799e347b7f778fb79450d6cadc29b038c1565ff0
Pavel <paha185@gamil.com>
2021-02-03 20:39:37 +0300

    Funcition lessons

--- js/New.js
+++ js/New.js
@@ -4,4 +4,16 @@ function showFirstMessahe(){
     console.log("I will buy new house, soon");
 }
 
-showFirstMessahe();
+///showFirstMessahe();
+
+//let calc =(a,b)=>a+b;
+
+//console.log(calc(5,5));
+
+
+const str="I will buy new house soon";
+
+console.log(str.substring(15));
+
+const num = "12.2px";
+console.log(parseFloat(num));
--- js/script.js
+++ js/script.js
@@ -1,32 +1,68 @@
-const  numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?");
+'use strict';
 
-const personalMovieDB ={
-    count: numberOfFilms,
-    movies : {},
-    actors: {},
-    genres:[],
-    private: false
+let numberOfFilms;
+
+
+function start() {
+  numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+
+  while (numberOfFilms == "" || numberOfFilms == null || isNaN(numberOfFilms)) {
+    numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+  }
+}
+
+start();
+
+const personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  private: false
 };
 
-      for(let i=0; i<2; i++)
-      {
-        const a = prompt("Один из последних просмотренных фильмов?",""),
-              b = prompt("На сколько оцените его?");
 
-        if(a != null && b != null && a != '' && b != '' && a.length < 50 ){
-            personalMovieDB.movies[a]=b;
-            console.log("Status: OK");
-        } else{
-            console.log("Error data not avaliable");
-            i--;
-        }
-      }
+function rememberMyFilms() {
+  for (let i = 0; i < 2; i++) {
+    const a = prompt("Один из последних просмотренных фильмов?", ""),
+          b = prompt("На сколько оцените его?");
 
-      let message = (personalMovieDB.count <= 10 ) ? 'Просмотрено довольно мало фильмов':
-        (personalMovieDB.count >10 && personalMovieDB.count <= 30) ? 'Вы класический зритель':
-        (personalMovieDB.count > 30) ? 'Вы киноман':
-        'произошла ошибка';
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+      personalMovieDB.movies[a] = b;
+      console.log("Status: OK");
+    } else {
+      console.log("Error data not avaliable");
+      i--;
+    }
+  }
+}
 
-        alert(message);
+//rememberMyFilms();
 
-      console.log(personalMovieDB);
+function detectedPersonalLevel() {
+  let message = (personalMovieDB.count <= 10) ? 'Просмотрено довольно мало фильмов' :
+    (personalMovieDB.count > 10 && personalMovieDB.count <= 30) ? 'Вы класический зритель' :
+    (personalMovieDB.count > 30) ? 'Вы киноман' :
+    'произошла ошибка';
+
+  alert(message);
+}
+
+//rememberMyFilms();
+//detectedPersonalLevel();
+writeYourGenres();
+showMyDB();
+
+
+
+function showMyDB() {
+  if (!personalMovieDB.private)
+    console.log(personalMovieDB);
+}
+
+function writeYourGenres() {
+  for (let i = 1; i <= 3; i++) {
+    personalMovieDB.genres[i - 1] = prompt(`Ваш любимый жанр под номером ${i}`);
+
+  }
+}
